7ddbb838fa7145ea453730ff91743b25af9fbfc6
Матвей Кусков <73180631+matveikuskov@users.noreply.github.com>
2022-12-23 11:15:20 +0300

    добавлен проект

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import Pagination from "./components/Pagination.vue";
+</script>
+
+<template>
+  <div id="app">
+    <header>
+      <div class="wrapper">
+        <HelloWorld msg="Image Gallery" />
+      </div>
+    </header>
+
+    <RouterView />
+    <Pagination />
+  </div>
+</template>
+
+<style scoped>
+#app {
+  display: block;
+  padding: 44px;
+}
+
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+  margin-bottom: 44px;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="pages-count">
+        <button>&lt;</button>
+        <button>1</button>
+        <button>2</button>
+        <button>...</button>
+        <button>></button>
+    </div>
+</template>
+
+<style scoped>
+.pages-count {
+    margin-top: 44px;
+    font-size: 18px;
+    text-align: right;
+}
+
+button {
+    padding: 12px 18px 12px 18px;
+    background-color: rgb(255, 255, 255);
+    border: 1px solid black;
+}
+
+button:first-child {
+    border-radius: 10px 0 0 10px;
+}
+
+button:last-child {
+    border-radius: 0 10px 10px 0;
+}
+
+button:hover {
+    cursor: pointer;
+    background-color: rgb(0, 0, 0);
+    color: rgb(255, 255, 255);
+}
+
+@media (max-width: 660px) {
+    .pages-count {
+    margin-top: 44px;
+    font-size: 18px;
+    text-align: center;
+}
+    button {
+        padding: 20px 28px 20px 28px;
+        background-color: rgb(255, 255, 255);
+        border: 1px solid black;
+    }
+}
+</style>
