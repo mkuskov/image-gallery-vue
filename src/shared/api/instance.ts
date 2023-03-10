a5b17e65101dbd979a20a9884b9186958776baa0
Матвей Кусков <73180631+mkuskov@users.noreply.github.com>
2023-03-10 17:06:02 +0300

    Update instance.ts

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  headers: {
-    Accept: "application/json",
-  },
+  headers: {"X-Master-Key", "$2b$10$6zPD17o0u8kKhqH6XM16IO4X8OUfdVyTd8nRmgTPTr0g3RbU7a9HC"}
 });
